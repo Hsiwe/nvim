303b646ba336397e72dfb0bb3c21943faa6630a0
hsiwe <46127681+Hsiwe@users.noreply.github.com>
2025-07-20 23:17:50 +0400

    set fugitive line blaming to false

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -16,7 +16,7 @@ require("gitsigns").setup({
 	},
 	auto_attach = true,
 	attach_to_untracked = false,
-	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -79,6 +79,5 @@ require("gitsigns").setup({
 		map("n", "<leader>hD", function()
 			gs.diffthis("~")
 		end)
-		map("n", "<leader>td", gs.toggle_deleted)
 	end,
 })
